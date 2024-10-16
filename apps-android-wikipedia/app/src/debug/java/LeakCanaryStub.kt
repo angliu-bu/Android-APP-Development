f654c21fae1c98a234714698fc5d819021a26ab3
Ang Liu <87093228+angliu-bu@users.noreply.github.com>
2024-10-15 21:23:14 -0700

    Delete apps-android-wikipedia directory

--- apps-android-wikipedia/app/src/debug/java/LeakCanaryStub.kt
+++
@@ -1,9 +0,0 @@
-package org.wikipedia
-
-import leakcanary.LeakCanary
-import org.wikipedia.settings.Prefs
-
-fun setupLeakCanary() {
-    val enabled = Prefs.isMemoryLeakTestEnabled
-    LeakCanary.config = LeakCanary.config.copy(dumpHeap = enabled)
-}
